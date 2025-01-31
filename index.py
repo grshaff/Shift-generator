675a19abf626c1099ea8a2fea14a9872584e2d23
grshaff <gshaffaat@gmail.com>
2025-01-31 11:37:21 +0700

    feat: finishing GUI

--- index.py
+++ index.py
@@ -30,7 +30,7 @@ def create_widgets():
     root.logLabel = CTkLabel(root, text="Names:")
     root.logLabel.place(x=30, y=165)
 
-    root.logFrame = st.ScrolledText(root, wrap=tk.WORD, width=25, height=7, font=("Montserrat", 10)) 
+    root.logFrame = st.ScrolledText(root, wrap=tk.WORD, width=25, height=8, font=("Montserrat", 10)) 
     root.logFrame.place(x=45, y=295)
     root.logFrame.configure(state ='disabled') 
 
@@ -41,20 +41,24 @@ def create_widgets():
     root.daycomBox = CTkComboBox(root, values=['28', '29', '30', '31'], width=80,)
     root.daycomBox.place(x=210, y=130)
 
+    # rules Button
+    root.GenerateBTN = CTkButton(root, text="Rules", command='', width=120, fg_color="gray", corner_radius=32)
+    root.GenerateBTN.place(x=190, y=195)
+
     # clear Button
-    root.GenerateBTN = CTkButton(root, text="Clear", command='', width=100, fg_color="gray", corner_radius=32)
-    root.GenerateBTN.place(x=30, y=290)
+    root.GenerateBTN = CTkButton(root, text="Clear", command='', width=120, fg_color="red", corner_radius=32)
+    root.GenerateBTN.place(x=190, y=225)
 
     # generate Button
-    root.GenerateBTN = CTkButton(root, text="Generate", command='', width=100, fg_color="green", corner_radius=32)
-    root.GenerateBTN.place(x=30, y=320)
+    root.GenerateBTN = CTkButton(root, text="Generate", command='', width=120, fg_color="green", corner_radius=32)
+    root.GenerateBTN.place(x=190, y=255)
 
 # Buat objek class tk
 root = CTk()
 
 # Setting ukuran judul
 root.title("Managed Service Shift Generator")
-root.geometry("350x400")
+root.geometry("340x320")
 root.resizable(False, False)
 set_appearance_mode("light")
 
